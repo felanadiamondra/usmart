0212816abef1c733df78391e7fa4013b5f9a9d10
AriEru10 <arielle.mtsiazonavo@gmail.com>
2023-03-02 23:38:59 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import UserMainPage from './Pages/UserPage/UserMainPage';
+import {UserMainPage} from './Pages/UserPage/UserMainPage';
+import {Login} from "./components/Login/Login";
+import {Registration} from "./components/Register/Register";
 
 function App() {
   return (
     <div className="App">
+      <Login/>
       <UserMainPage/>
+      <Registration/>
     </div>
   );
 }
